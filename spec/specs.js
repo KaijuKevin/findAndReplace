4c9fbf6bae545e14526ecf97461a70f36c6f6727
Kevin Chen <kevc.dev@gmail.com>
2015-05-29 10:32:34 -0700

    Test a larger text block

--- spec/specs.js
+++ spec/specs.js
@@ -11,4 +11,8 @@ describe("findReplace", function(){
 		expect(findReplace("to too two", "t", "w")).to.equal("wo woo wwo");
 	});
 
+	it("will replace multiple matches in a large string", function() {
+		var textBlock = "Style never met and those among great. At no or December sportsmen he perfectly happiness attending. Depending listening delivered off new she procuring satisfied sex existence. Person plenty answer to exeter it if. Law use assistance especially resolution cultivated did out sentiments unsatiable. Way necessary had intention happiness but December delighted his curiosity. Furniture furnished or on strangers neglected remainder engrossed.";
+		expect(findReplace(textBlock, "December", "september")).to.eq("Style never met and those among great. At no or september sportsmen he perfectly happiness attending. Depending listening delivered off new she procuring satisfied sex existence. Person plenty answer to exeter it if. Law use assistance especially resolution cultivated did out sentiments unsatiable. Way necessary had intention happiness but september delighted his curiosity. Furniture furnished or on strangers neglected remainder engrossed.");
+	});
 });
