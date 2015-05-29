7e39215cab32385bd2212d35b6f848ab5666b3e2
Kevin Chen <kevc.dev@gmail.com>
2015-05-29 10:58:07 -0700

    Display form results on page

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,18 @@ function findReplace(textBlock, targetWord, replacement) {
 	var editBlock = textBlock;
 	while( editBlock.indexOf(targetWord) >= 0 ) {
 		editBlock = editBlock.replace(targetWord, replacement);
-	}
+	};
 	return editBlock;
 };
+
+$(function(){
+	$("form#findAndReplace").submit(function(event) {
+		var textBlock = $("textarea#textBlock").val();
+		var targetWord = $("input#targetWord").val();
+		var replacement = $("input#replacement").val();
+		var results = findReplace(textBlock, targetWord, replacement);
+		$("p#results").text(results);
+
+		event.preventDefault();
+	});
+});
